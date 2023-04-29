a237d698d7724dba46c8d9e0d8227e05a750a933
Ali Küçükkaya <108999321+ExuviaeSubarashii@users.noreply.github.com>
2023-04-29 08:05:25 +0300

    Update BluetoothConnectionService.java

--- app/src/main/java/com/example/thisshouldwork/BluetoothConnectionService.java
+++ app/src/main/java/com/example/thisshouldwork/BluetoothConnectionService.java
@@ -17,10 +17,6 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.UUID;
 
-/**
- * Created by User on 12/21/2016.
- */
-
 public class BluetoothConnectionService {
     private static final String TAG = "BluetoothConnectionServ";
 
